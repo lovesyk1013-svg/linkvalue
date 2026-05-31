817549a53367f6de217275e0e655d5532dcf6fec
lovesyk1013-svg <lovesyk1013@gmail.com>
2026-05-31 18:08:43 +0900

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ohuysnrtmyqmmqojrtyi.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9odXlzbnJ0bXlxbW1xb2pydHlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyOTIxNjAsImV4cCI6MjA2Mzg2ODE2MH0.Z5wNMOGwkItbWr6KGX6e3WihhQlHiVtHx6em8Jjhr5Y'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
